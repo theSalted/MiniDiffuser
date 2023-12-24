d16599b6583b1a379fcceb4560d8cffb89771749
Yuhao Chen <yuhao.chen2000@icloud.com>
2023-12-24 19:29:31 +0800

    created mini diffuser model - an improved result export system

--- minid.py
+++ minid.py
@@ -4,6 +4,24 @@ from torchvision import transforms
 from diffusers import UNet2DModel
 from torch.optim import Adam
 
+import os
+from datetime import datetime
+
+# UTILITIES
+def generate_unique_folder_name(base_folder):
+    """Function to generate a unique folder name by adding a number if it already exists"""
+    counter = 1
+    unique_folder = base_folder
+    while os.path.exists(unique_folder):
+        unique_folder = f"{base_folder}-{counter}"
+        counter += 1
+    return unique_folder + '/'
+
+def generate_folder(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder, mode=0o777, exist_ok=True)
+
+# MODELS
 def get_model():
     block_out_channels=(128, 128, 256, 256, 512, 512)
     down_block_types=( 
@@ -25,7 +43,7 @@ def get_model():
     return UNet2DModel(block_out_channels=block_out_channels,out_channels=3, in_channels=3, up_block_types=up_block_types, down_block_types=down_block_types, add_attention=True)
 
 @torch.no_grad()
-def sample_iadb(model, x0, nb_step):
+def iadb(model, x0, nb_step):
     x_alpha = x0
     for t in range(nb_step):
         alpha_start = (t/nb_step)
@@ -36,7 +54,7 @@ def sample_iadb(model, x0, nb_step):
 
     return x_alpha
 
-
+# MAIN
 device = torch.device("cpu")
 if torch.cuda.is_available():
     device = torch.device("cuda:0")
@@ -44,12 +62,20 @@ if torch.cuda.is_available():
 elif torch.backends.mps.is_available():
     device = torch.device("mps")
     print("MPS is available")
-        
+
+# Generating a base folder name
+current_time = datetime.now()
+model_name = current_time.strftime('%y%m%d') + '-cifar-fp32'
+base_folder = './results/' + model_name
+
+# Generate folders
 DATASET_FOLDER = './datasets/cifar10/'
+RESULT_FOLDER = generate_unique_folder_name(base_folder)
+
 transform = transforms.Compose([transforms.Resize(32),transforms.CenterCrop(32), transforms.RandomHorizontalFlip(0.5),transforms.ToTensor()])
 train_dataset = torchvision.datasets.CIFAR10(root=DATASET_FOLDER, train=True,
                                         download=True, transform=transform)
-
+                                        
 dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=0, drop_last=True)
 
 model = get_model()
@@ -57,6 +83,7 @@ model = model.to(device)
 
 optimizer = Adam(model.parameters(), lr=1e-4)
 nb_iter = 0
+
 print('Start training')
 for current_epoch in range(100):
     for i, data in enumerate(dataloader):
@@ -77,7 +104,8 @@ for current_epoch in range(100):
 
         if nb_iter % 200 == 0:
             with torch.no_grad():
-                print(f'Save export {nb_iter}')
-                sample = (sample_iadb(model, x0, nb_step=128) * 0.5) + 0.5
-                torchvision.utils.save_image(sample, f'export_{str(nb_iter).zfill(8)}.png')
-                torch.save(model.state_dict(), f'iadb_cifar10_default.ckpt')
+                generate_folder(RESULT_FOLDER)
+                print(f'Save export {nb_iter} - loss {loss}')
+                sample = (iadb(model, x0, nb_step=128) * 0.5) + 0.5
+                torchvision.utils.save_image(sample, f'{RESULT_FOLDER}preview_{str(nb_iter).zfill(8)}.png')
+                torch.save(model.state_dict(), f'{RESULT_FOLDER}weights.ckpt')
